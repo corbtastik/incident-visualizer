17b1560306f14ef9eb040d5a487d318aacbe2556
Corbett Martin <corbtastik@gmail.com>
2026-08-24 14:36:21 -0500

    fix: update media attachment icon to monochrome SVG style
    Replace paperclip emoji with SVG icon matching other UI icons

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -58,7 +58,9 @@ function MediaAttachment({ media }) {
   return (
     <div className="sr-row__media">
       <div className="sr-row__media-icon" title="Has attached media">
-        📎
+        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+          <path d="M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66l-9.2 9.19a2 2 0 0 1-2.83-2.83l8.49-8.48" />
+        </svg>
       </div>
       <div className="sr-row__media-info">
         <span className="sr-row__media-filename">{filename}</span>
